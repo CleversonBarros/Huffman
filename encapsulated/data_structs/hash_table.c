988b6c6616e63638a27aca7d3d9fd9c60f4e67f6
Lael Santa Rosa <lael_lsr@hotmail.com>
2018-04-26 14:28:02 -0300

    Update hash_table.c

--- encapsulated/data_structs/hash_table.c
+++ encapsulated/data_structs/hash_table.c
@@ -3,5 +3,5 @@
 void create_empty_table(unsigned char code_map[][9])
 {
 	int lines;
-	for(lines = 0; lines < 256; i++) code_map[lines][0] = '\0';
+	for(lines = 0; lines < 256; lines++) code_map[lines][0] = '\0';
 }
